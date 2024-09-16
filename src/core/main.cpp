c46f7621faa196556443ee82e89233635a4e7037
Schepdog <jschepis02@gmail.com>
2024-09-17 03:14:38 +1000

    deleted build dir. still trying to gitignore this thing

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -77,6 +77,8 @@ GLuint loadTextureFromFile(const std::string& path) {
     glGenTextures(1, &textureId);
     glBindTexture(GL_TEXTURE_2D, textureId);
 
+    std::cout << "something" << std::endl;
+
     int width, height, nrChannels;
     unsigned char* data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
     //std::cout << data << std::endl;
